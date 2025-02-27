abd41918a7905a9e4f3ece0702ff676e5f898cf4
Juan <jurbina148@unab.edu.co>
2025-02-27 12:23:14 -0500

    Routes
    rutas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    //Listado de productos
+    return view('products');
+});
+
+Route::get('/products/create', function () {
+    //Crear producto
+    return view('create');
+});
+
+Route::get('/products/{id}', function ($id) {
+    //Detalle de producto
+    return view('product');
+});
+
+Route::get('/products/{id}/{category}', function ($id, $category) {
+    //Detalle de categoria
+    return view('category');
+});
+
